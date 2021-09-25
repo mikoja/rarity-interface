7514a35a65b955638a5094f75580e0fe6a37bc10
Mikko <mikko.ojala@mail.com>
2021-09-25 13:29:52 +0300

    Kinesis

--- src/rarityConfig.ts
+++ src/rarityConfig.ts
@@ -17,7 +17,18 @@ interface Config {
 }
 
 const config: Config = {
-  contractAddress: '',
+  contractAddress: '0xeb113c5d09bfc3a7b27a75da4432fb3484f90c6a',
+  headerText: 'Kinesis: Atlanta',
+  links: {
+    website: 'https://kinesis.art/',
+    twitter: 'https://twitter.com/_kinesisart',
+    discord: 'https://discord.gg/xvWNSBZA7J',
+    etherscan:
+      'https://etherscan.io/address/0xeb113c5d09bfc3a7b27a75da4432fb3484f90c6a',
+    openSea: 'https://opensea.io/collection/kinesisart',
+  },
+  mainImageKey: 'animation_url',
+  logoURL: 'https://lh3.googleusercontent.com/FeiRpf4rUf6ITYZb6b311aANPff_d_POCiIHpJkA9sRQUav-S0zNlqgs90xkwUNkJUlc6se1pDCvFjyLmxZ2KSOei_MC7UHjuXzE=s130',
 }
 
 export default config
